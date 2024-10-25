6b6b21097bc308c90ea255d228a5f0f48eb94afa
MojtabaBRZ <MojtabaBahrami147@gmail.com>
2024-10-25 23:37:34 +0330

    Optimal control in progress

---
+++ Control/Optimal Control/Examples/LQR-DP.py
@@ -0,0 +1,48 @@
+import numpy as np
+from control import dlqr
+
+A = np.array([[0.9, 0.01], [-0.2, 0.98]])
+B = np.array([[0],[0.5]])
+
+Q = np.ones(np.shape(A))
+R = np.array([[0.1]])
+
+K, _, _ = dlqr(A, B, Q, R)
+print("DLAR solution : ", K)
+
+# To solve LQR problem using DP and value iteration, you must calculate K_next until convergence
+# P_next = (A - BK)'P(A - BK) + Q + K'RK
+# K_next = (R + B'P_nextB)^-1B'P_nextA
+n_itr = 0
+P = np.zeros(np.shape(A))
+K = np.array([[0, 0]])
+while(1):
+    P = (A - B @ K).T @ P @ (A - B @ K) + Q + K.T @ R @ K
+    pre_K = K
+    K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
+    
+    n_itr += 1
+    
+    if np.sum(np.abs(pre_K - K)) < 1e-6:
+        break
+
+print("DP solution : ", K, "   in ", n_itr, " iterations")
+
+n_itr = 0
+P = np.zeros(np.shape(A))
+K = np.array([[0, 0]])
+while(1):
+    P_pre = P
+    for i in range(20):
+        P = (A - B @ K).T @ P @ (A - B @ K) + Q + K.T @ R @ K
+        if np.sum(np.abs(P_pre - P)) < 1e-4:
+            break
+    pre_K = K
+    K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
+    
+    n_itr += 1
+    
+    if np.sum(np.abs(pre_K - K)) < 1e-6:
+        break
+
+print("GPI solution : ", K, "   in ", n_itr, " iterations")
--- README.md
+++ README.md
@@ -6,8 +6,10 @@
 #### [Physics Informed Machine Learning](./Machine%20Learning/1.PhIML-An%20Introduction%20to%20Physics-Informed%20ML.md)
 
 ## Control
-#### [Modern Control Introduction](./Control/Modern%20Control%20Introduction.md)
-
+#### [Modern Control Introduction](./Control/Modern%20Control%20Introduction/Modern%20Control%20Introduction.md)
+#### [Model Predictive Control](./Control/MPC/Model%20Predictive%20Control.md)
+#### [Optimal Control Theory](./Control/Optimal%20Control/Optimal%20Control.md)
+* [Reinforcement Learning Application](./Control/Optimal%20Control/Optimal%20Control.md/#reinforcement-learning-application)
 ## Path Planning And Guidance
 
 ## Simulation
